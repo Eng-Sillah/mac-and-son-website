3a2571eda58d8568f61cb5fa5a4fd1758442011a
Your Name <your-email@example.com>
2024-01-15 22:53:41 +0000

    Making web to be responsive

--- src/app/ui/navbar/navbar.tsx
+++ src/app/ui/navbar/navbar.tsx
@@ -1,17 +1,30 @@
-import NavLinks from "./navlinks"
-import styles from "./navbar.module.css"
+"use client"
+
+import React, { useState } from "react";
+import NavLinks from "./navlinks";
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    return (
-        <div className={styles.container}>
-            <div>
-                <h1>LOGO</h1>
-            </div>
-            <div className={styles.links}>
-                <NavLinks />
-                <p>+23276711110</p>
-            </div>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className={`${styles.container} ${isMenuOpen ? styles.openMenu : ""}`}>
+      <div>
+        <h1>LOGO</h1>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.hamburgerMenu} onClick={toggleMenu}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
         </div>
-    )
+        <NavLinks />
+        <p>+23276711110</p>
+      </div>
+    </div>
+  );
 }
